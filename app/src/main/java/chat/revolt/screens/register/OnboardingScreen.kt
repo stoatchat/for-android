858c55a6b657e3597a48100275c6ab51265e3873
AbronStudio <teamabron@gmail.com>
2025-08-04 10:57:45 +0330

    Refactor: Update Onboarding and Settings screens
    - Removed unused imports and rearranged existing ones in `OnboardingScreen.kt` for better code organization.
    - Removed back navigation button from `SettingsScreen.kt` for a cleaner UI experience.

--- app/src/main/java/chat/revolt/screens/register/OnboardingScreen.kt
+++ app/src/main/java/chat/revolt/screens/register/OnboardingScreen.kt
@@ -8,13 +8,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.imeNestedScroll
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,9 +26,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import chat.revolt.R
