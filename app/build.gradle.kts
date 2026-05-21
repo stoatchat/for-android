9db2517ca609ee8a807b6ce4f7c576b9323a8683
infi <infi@infi.sh>
2026-05-21 22:18:58 +0200

    chore: bump to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         applicationId = "chat.revolt"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = Integer.parseInt("001_004_000".replace("_", ""), 10)
-        versionName = "1.4.0"
+        versionCode = Integer.parseInt("001_005_000".replace("_", ""), 10)
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
