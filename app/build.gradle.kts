feff13afc9570399c8bbdd675c3fb1d350ade981
infi <infi@infi.sh>
2026-07-31 01:34:32 +0200

    chore: bump to 1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         applicationId = "chat.revolt"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = Integer.parseInt("001_007_000".replace("_", ""), 10)
-        versionName = "1.7.0"
+        versionCode = Integer.parseInt("001_007_001".replace("_", ""), 10)
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
