adc73df83294e65e86bb1da09c2abbe6b1990f4b
infi <infi@infi.sh>
2026-08-04 12:02:53 +0200

    feat: clip active DM indicator to bounds

--- app/src/main/java/chat/stoat/composables/screens/chat/drawer/ChannelSideDrawer.kt
+++ app/src/main/java/chat/stoat/composables/screens/chat/drawer/ChannelSideDrawer.kt
@@ -60,6 +60,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
@@ -1009,6 +1010,7 @@ fun DMOrGroupItem(
             )
             .padding(vertical = 16.dp)
             .fillMaxWidth()
+            .clipToBounds()
             .then(
                 if (isMuted) {
                     Modifier.alpha(0.5f)
